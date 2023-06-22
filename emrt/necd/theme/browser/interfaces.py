0de14ad01dfa1591ae2281fd934cd35970e0aa52
David Batranu <david.batranu@eaudeweb.ro>
2023-06-22 19:35:46 +0300

    Migrating theme to Plone 6.

--- emrt/necd/theme/browser/interfaces.py
+++ emrt/necd/theme/browser/interfaces.py
@@ -1,8 +1,10 @@
 from plone.theme.interfaces import IDefaultPloneLayer
 
+
 class IThemeSpecific(IDefaultPloneLayer):
     """Marker interface that defines a Zope 3 browser layer.
-       If you need to register a viewlet only for the
-       "EMRT-NECD Theme" theme, this interface must be its layer
-       (in theme/viewlets/configure.zcml).
+
+    If you need to register a viewlet only for the
+    "EMRT-NECD Theme" theme, this interface must be its layer
+    (in theme/viewlets/configure.zcml).
     """
