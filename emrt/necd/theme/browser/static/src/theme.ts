07acdc3f4ed80c8158221666796d51255e9fe419
David Batranu <david.batranu@eaudeweb.ro>
2023-06-29 20:31:03 +0300

    Update theme.js (fix deprecated code). comments.pt moved to e.n.content.

--- emrt/necd/theme/browser/static/src/theme.ts
+++ emrt/necd/theme/browser/static/src/theme.ts
@@ -1,51 +1,59 @@
 import * as moment from "moment";
 
-function hightlight_restructured(){
-    if ($("body").hasClass("template-emrt-necd-content-observation") ||
-        $("body").hasClass("template-emrt-necd-content-conclusion") ||
-        $("body").hasClass("template-edit-highlights") ||
-        $("body").hasClass("template-edit portaltype-conclusion") ||
-        $("body").hasClass("template-emrt-necd-content-conclusion") ||
-        $("body").hasClass("template-emrt-necd-content-conclusionsphase2") ||
-        $("body").hasClass("template-edit portaltype-conclusionsphase2") ||
-        $("body").hasClass("template-view portaltype-observation")){
-            $("<br/><br/><span style='font-weight:bold'>Draft/final conclusion flags</span><br/>").insertBefore($("input[value='psi']").parent())
-    }
+function hightlight_restructured() {
+  const elBody = $("body");
+  if (
+    elBody.hasClass("template-emrt-necd-content-observation") ||
+    elBody.hasClass("template-emrt-necd-content-conclusion") ||
+    elBody.hasClass("template-edit-highlights") ||
+    elBody.hasClass("template-edit portaltype-conclusion") ||
+    elBody.hasClass("template-emrt-necd-content-conclusion") ||
+    elBody.hasClass("template-emrt-necd-content-conclusionsphase2") ||
+    elBody.hasClass("template-edit portaltype-conclusionsphase2") ||
+    elBody.hasClass("template-view portaltype-observation")
+  ) {
+    $(
+      "<br/><br/><span style='font-weight:bold'>Draft/final conclusion flags</span><br/>"
+    ).insertBefore($("input[value='psi']").parent());
+  }
 }
 
-$(document).ready(function(){
-
-    $(".collapsiblePanelTitle").click(function(e){
-        var panel = $(this).data("panel");
-        if ($(this).hasClass("collapsed")){
-            $("."+panel).show();
-            if (panel == "observation-workflow"){
-                $("#workflowTable").parent().scrollLeft($("#workflowTable").outerWidth())
-            }
-        }else{
-            $("."+panel).hide();
-        }
-        $(this).toggleClass("collapsed");
-    });
-    $(".collapsibleListTitle").click(function(e){
-        var list = $(this).data("list");
-        if ($(this).hasClass("collapsed")){
-            $("."+list).show();
-        }else{
-            $("."+list).hide();
-        }
-        $(this).toggleClass("collapsed");
-    });
-	$(".clickableRow").click(function(evt) {
-        window.open($(this).data("href"), "_blank");
-	});
-	$(".datetimeWF").each(function(){
-		var time = $.trim($(this).text());
-        var timeAgo = moment(time, "YYYY/MM/DD HH:mm:ss").format("YYYY/MM/DD HH:mm:ss")
-        var timeZone = time.substr(time.indexOf("+", time.length))
-        timeAgo += " +0" + timeZone + ":00"
-		$(this).text(moment(timeAgo, "YYYY/MM/DD HH:mm:ss Z").fromNow())
-	});
-    hightlight_restructured();
-    $('a.standardButton[title][title!=""]').addClass("tooltipIcon");
+$(function () {
+  $(".collapsiblePanelTitle").on("click", function (e) {
+    var panel = $(this).data("panel");
+    if ($(this).hasClass("collapsed")) {
+      $("." + panel).show();
+      if (panel == "observation-workflow") {
+        $("#workflowTable")
+          .parent()
+          .scrollLeft($("#workflowTable").outerWidth());
+      }
+    } else {
+      $("." + panel).hide();
+    }
+    $(this).toggleClass("collapsed");
+  });
+  $(".collapsibleListTitle").on("click", function (e) {
+    var list = $(this).data("list");
+    if ($(this).hasClass("collapsed")) {
+      $("." + list).show();
+    } else {
+      $("." + list).hide();
+    }
+    $(this).toggleClass("collapsed");
+  });
+  $(".clickableRow").on("click", function (evt) {
+    window.open($(this).data("href"), "_blank");
+  });
+  $(".datetimeWF").each(function () {
+    var time = $(this).text().trim();
+    var timeAgo = moment(time, "YYYY/MM/DD HH:mm:ss").format(
+      "YYYY/MM/DD HH:mm:ss"
+    );
+    var timeZone = time.substring(time.indexOf("+") + 1);
+    timeAgo += " +0" + timeZone + ":00";
+    $(this).text(moment(timeAgo, "YYYY/MM/DD HH:mm:ss Z").fromNow());
+  });
+  hightlight_restructured();
+  $('a.standardButton[title][title!=""]').addClass("tooltipIcon");
 });
